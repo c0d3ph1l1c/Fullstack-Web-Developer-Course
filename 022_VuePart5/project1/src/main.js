c0a02cfe8313d6a6777e507edfd665e688c6d9fe
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2020-03-09 14:51:38 +0800

    Completed Vue Part 5
     	modified:   022_VuePart5/project1/package-lock.json
     	modified:   022_VuePart5/project1/package.json
     	modified:   022_VuePart5/project1/src/App.vue
     	deleted:    022_VuePart5/project1/src/assets/logo.png
     	new file:   022_VuePart5/project1/src/components/Cmp1.vue
     	deleted:    022_VuePart5/project1/src/components/HelloWorld.vue
     	new file:   022_VuePart5/project1/src/components/Index.vue
     	new file:   022_VuePart5/project1/src/components/common/Table.vue
     	modified:   022_VuePart5/project1/src/main.js
     	modified:   022_VuePart5/project1/src/router/index.js

--- 022_VuePart5/project1/src/main.js
+++ 022_VuePart5/project1/src/main.js
@@ -6,10 +6,39 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// vuex1
+import Vuex from 'vuex';
+
+// vuex2 - install into Vue
+Vue.use(Vuex);
+
+// vuex3 - Declare store object
+const store = new Vuex.Store({
+  // Prevent state to be modified directly
+  strict: process.env.NODE_ENV != 'production',   
+  state: {a: 12, b: 5},       // Core: Data
+  mutations: {
+    add(state, n) {
+      state.a += n; 
+    }
+  },
+  actions: {
+    add({commit}, n) {
+      commit('add', n);
+    }
+  },
+  getters: {},
+  modules: {}    
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
